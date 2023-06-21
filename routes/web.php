0400d8361fec0234803d0a31233834b2a1517fd9
anindyaha <anindyaha.official@gmail.com>
2023-06-21 22:50:20 +0700

    Employee Clone

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\ProductController;
 
 /*
